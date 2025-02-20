27d12fb0572c719faf857083744cd3a43f2141e9
LukaOstojic26 <lukalbj26@gmail.com>
2025-02-20 17:26:16 +0100

    popravljen signal tau

--- acrobot_model!.jl
+++ acrobot_model!.jl
@@ -1,5 +1,5 @@
 function acrobot_model!(dx, x, p, t)
-    I1, I2, m1, m2, l1, l2, lc1, lc2 = p;
+    I1, I2, m1, m2, l1, l2, lc1, lc2, tau = p;
     g = 9.81;
 
     G1 = I1 + m1*lc1^2 + m2*l1^2;
@@ -11,7 +11,7 @@ function acrobot_model!(dx, x, p, t)
     M = [0 0 1 0;
          0 0 0 1];
     T = [0;
-         0];  # T = [0; tau]
+         tau(t)];  
     C = [(-2*G3*x[4]*sin(x[2])) (-G3*x[4]*sin(x[2]));
           G3*x[3]*sin(x[2])              0           ];
     G = [G4*cos(x[1]) + G5*cos(x[1] + x[2]);
