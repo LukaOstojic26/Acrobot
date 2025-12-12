be49dea0cfa9dc3e2cd02665a7b0c6943645a943
LukaOstojic26 <lukalbj26@gmail.com>
2025-12-12 14:14:25 +0100

    dodato plotovanje sile reakcije u prvom zglobu

--- lqr_demo.jl
+++ lqr_demo.jl
@@ -1,9 +1,11 @@
 using DifferentialEquations
 using ControlSystems
+using Plots
 
 include("acrobot_model!.jl")
 include("animate_acrobot.jl")
 include("linear_acrobot.jl")
+include("reaction_forces.jl")
 
 
 I1, I2, m1, m2, l1, l2, lc1, lc2 = (0.00192579089, 0.0281182992, 0.34111236, 2.1601915299999996, 0.3375, 0.4087, 0.240403710, 0.211247879);
@@ -25,5 +27,15 @@ K = lqr(Continuous, A, B, Q, R)
 param = (I1, I2, m1, m2, l1, l2, lc1, lc2, K);
 prob = ODEProblem(acrobot_model!, x0, tspan, param);
 sol = solve(prob);
+
+####### Plotovanje sile reakcije #######
+Fr = reaction_forces(sol, param)
+
+Frx = [row[1] for row in Fr]
+Fry = [row[2] for row in Fr]
+
+Plots.plot(sol.t, Frx, label="Fx", color=:blue, linewidth=3)
+Plots.plot!(sol.t, Fry, label="Fy", color=:red, linewidth=3)
+########################################
 
 #acrobot_animation(sol, x0, param)
--- reaction_forces.jl
+++ reaction_forces.jl
@@ -1,54 +1,43 @@
 using Plots
 
 include("acrobot_model!.jl")
-
-function x_dot(sol, p)
-    dx = []
-    for (ti, ui) in zip(sol.t, sol.u)
-        du = similar(ui)
-        acrobot_model!(du, ui, p, ti)
-        push!(dx, du)
-    end
-
-    return dx
-end
+include("control.jl")
 
 function reaction_forces(sol, p)
     I1, I2, m1, m2, l1, l2, lc1, lc2, K = p;
-    dx = x_dot(sol, p)
+    g = 9.81;
+    m = m1 + m2
 
-    q1 = [el[1] for el in sol.u]
-    q2 = [el[2] for el in sol.u]
-    q1_d = [el[3] for el in sol.u]
-    q2_d = [el[4] for el in sol.u]
-    q1_dd = [el[3] for el in dx]
-    q2_dd = [el[4] for el in dx]
+    G1 = I1 + m1*lc1^2 + m2*l1^2;
+    G2 = I2 + m2*lc2^2;
+    G3 = m2*l1*lc2;
+    G4 = g*(m1*lc1 + m2*l1);
+    G5 = m2*g*lc2;
 
+    Fr = []
 
-    ac2x = -l1.*q1_dd.*sin.(q1) .- lc2.*(q1_dd .+ q2_dd).*sin.(q1 .+ q2) .- l1.*(q1_d .^ 2).*cos.(q1) 
-                                                                                    .- lc2.*((q1_d .+ q2_d).^2).*cos.(q1 .+ q2)  
-    ac2y = l1.*q1_dd.*cos.(q1) .+ lc2.*(q1_dd .+ q2_dd).*cos.(q1 .+ q2) .- l1.*(q1_d .^ 2).*sin.(q1) 
-                                                                                    .- lc2.*((q1_d .+ q2_d).^2).*sin.(q1 .+ q2)
-    ac1x = -lc1.*(q1_d.^2).*cos.(q1)
-    ac1y = -lc1.*(q1_d.^2).*sin.(q1)
+    for x in sol.u
 
-    A = 1 ./ (2 .* cos.(q1))
-    B = A .* (-2 .* m2 .* ac2x .* sin.(q1) .- I1.*q1_d .+ m1.*9.81 .* cos.(q1) 
-                                                                .+ m1.*ac1y.*cos.(q1) .+ m1.*ac1x.*sin.(q1))
-    C = -1 ./ (sin.(q1 .+ q2))
-    D = m1.*ac1x .+ C .* (m2.*ac2y.*cos.(q1 .+ q2) .+ m2*9.81 .* cos.(q1 .+ q2) + I2.*(q1_d .+ q2_d))
+        M = [0 0 1 0;
+             0 0 0 1];
+        T = [0;
+             lqr_control(x, K)];  
+        C = [(-2*G3*x[4]*sin(x[2])) (-G3*x[4]*sin(x[2]));
+              G3*x[3]*sin(x[2])              0           ];
+        G = [G4*cos(x[1]) + G5*cos(x[1] + x[2]);
+                    G5*cos(x[1] + x[2])        ];
+        H⁻¹ = (1/(G1*G2 - (G3^2)*(cos(x[2])^2))) .* [G2 (-G2 - G3*cos(x[2])); (-G2 - G3*cos(x[2])) (G1 + G2 + 2*G3*cos(x[2]))]; 
+        Jc = [((-m1/m)*lc1*sin(x[1]) - (m2/m)*l1*sin(x[1]) - (m2/m)*lc2*sin(x[1] + x[2])) ((-m2/m)*lc2*sin(x[1] + x[2]));
+              ((m1/m)*lc1*cos(x[1]) + (m2/m)*l1*cos(x[1]) + (m2/m)*lc2*cos(x[1] + x[2]))  ((m2/m)*lc2*cos(x[1] + x[2]))]
+        Jc_d = [((-m1/m)*lc1*x[3]*cos(x[1]) - (m2/m)*l1*x[3]*cos(x[1]) - (m2/m)*lc2*(x[3] + x[4])*cos(x[1] + x[2])) ((-m2/m)*lc2*(x[3] + x[4])*cos(x[1] + x[2]));
+                ((-m1/m)*lc1*x[3]*sin(x[1]) - (m2/m)*l1*x[3]*sin(x[1]) - (m2/m)*lc2*(x[3] + x[4])*sin(x[1] + x[2])) ((-m2/m)*lc2*(x[3] + x[4])*sin(x[1] + x[2]))]
+        
+        F = ((m.*Jc)*H⁻¹)*T + (m.*(Jc_d - Jc*H⁻¹*C))*(M*x) - m.*(Jc*H⁻¹*G) - m.*[0; g]
 
-    x = [ [w, i, y, z] for (w, i, y, z) in zip(q1, q2, q1_d, q2_d)]
+        push!(Fr, F)
 
-    tau = []
-    for xi in x 
-        tau_i = dot(-K, xi - [pi/2, 0, 0, 0])
-        push!(tau, tau_i)
     end
 
-    Fx = C .* tau .+ D
-    Fy = A .* tau .+ B
-
-    Plots.plot(sol.t, [Fx Fy], title="Sile reakcije", label=["Fx" "Fy"], linewidth=3, ylims=(-30, 31))
+    return Fr
 
 end
